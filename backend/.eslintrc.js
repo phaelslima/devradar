b0691af1d9ec1e8c04e523be2c932d0d3e39291b
Raphael Lima <phaelslima@gmail.com>
2020-09-02 07:12:33 -0300

    feat(backend): add eslint and prettier

---
+++ backend/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ['airbnb-base', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    camelcase: 'off',
+  },
+};
