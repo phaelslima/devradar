d4f48efce6b6859c8792058998fd559f2415a3e5
Raphael Lima <phaelslima@gmail.com>
2020-09-05 17:11:39 -0300

    feat(backend): socket config

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const http = require('http');
 
 const routes = require('./routes');
+const { setupWebsocket } = require('./websocket');
 
 require('dotenv').config();
 
 const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
 
 mongoose.connect(process.env.MONGODB_URL, {
   useNewUrlParser: true,
@@ -19,4 +24,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(process.env.PORT || 3333);
+server.listen(process.env.PORT || 3333);
