06d71e337e0ebc2da5fbf47cf125780fe269dfb5
Raphael Lima <phaelslima@gmail.com>
2020-09-04 22:35:43 -0300

    feat(backend): add cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
 const routes = require('./routes');
 
 require('dotenv').config();
@@ -13,6 +15,7 @@ mongoose.connect(process.env.MONGODB_URL, {
   useCreateIndex: true,
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
